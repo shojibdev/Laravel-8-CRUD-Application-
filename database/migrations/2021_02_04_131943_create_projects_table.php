b989cd056e11e4ddc65eb957b3f19b6e548f3948
Shojib <khshogib44@gmail.com>
2021-02-06 15:09:39 +0600

    first commit

---
+++ database/migrations/2021_02_04_131943_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255)->nullable();
+            $table->string('introduction',255)->nullable();
+            $table->string('location',255)->nullable();
+            $table->decimal('cost',22)->nullable()->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
